ae6b2303bd8391820461ffe57e3e40d6eef97054
Jonathan Tremesaygues <jonathan.tremesaygues@slaanesh.org>
2022-11-28 19:24:07 +0100

    add vuejs helloworld

---
+++ configure.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+
+
+
+
+
+def main() -> None:
+    pass
+
+
+if __name__ == "__main__":
+    main()
--- shell.nix
+++ shell.nix
@@ -1,4 +1,4 @@
 { pkgs ? import <nixpkgs> {} }:
 pkgs.mkShell {
-  nativeBuildInputs = with pkgs; [ yosys nodejs ];
+  nativeBuildInputs = with pkgs; [ nodejs python310 yosys ];
 }
